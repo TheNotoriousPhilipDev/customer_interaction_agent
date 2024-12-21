503529d1bbc021a420667ce6ec86ceb11cfc6087
andres <afgomez0622@gmail.com>
2024-12-21 18:00:20 -0500

    initialized project

---
+++ src/main/java/org/agomez/msvc/cia/MsvcCiaApplication.java
@@ -0,0 +1,13 @@
+package org.agomez.msvc.cia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcCiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/agomez/msvc/cia/MsvcCiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agomez.msvc.cia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
